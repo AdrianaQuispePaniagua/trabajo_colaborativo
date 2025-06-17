c5dca09baaf70caa7be23268192014413a765b52
AdrianaQuispePaniagua <araninha1994@gmail.com>
2025-06-17 01:42:18 -0400

    feat/rama-adriana: estructura del menu principal, control de flujo y definicion de funciones.

---
+++ functions.py
@@ -0,0 +1,15 @@
+def agregar_contacto(lista_contactos):
+    # TODO: logica agregar contacto. Borrar 'pass'
+    pass
+
+def mostrar_contactos(lista_contactos):
+    # TODO: logica mostrar todos los contactos. Borrar 'pass'
+    pass
+
+def buscar_contacto(lista_contactos, nombre_buscado):
+    # TODO: logica buscar 'nombre_buscado' en lista_contactos. Borrar 'pass'
+    pass
+
+def eliminar_contacto(lista_contactos, nombre_eliminar):
+    # TODO: logica eliminar 'nombre_eliminar' de lista_contactos. Borrar 'pass'
+    pass
